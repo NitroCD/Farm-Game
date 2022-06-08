07d957429d27e100db4e0a943fd3c19992162440
NitroCD <cdhansma@gmail.com>
2022-06-08 11:23:21 -0600

    More comments

--- Assets/Scripts/Build.cs
+++ Assets/Scripts/Build.cs
@@ -44,7 +44,7 @@ public class Build : MonoBehaviour
     {
         return tileStorage;
     }
-
+    //Called whenever the player wants to place a tile (Clicks on a tile button)
     public void SetBuildInfo()
     {
         if(!isBuilt)
@@ -72,7 +72,7 @@ public class Build : MonoBehaviour
             }
         }
     }
-
+    //Called from SetBuildInfo and GameManager.LoadTiles, handles the instantiation of a tile when the player builds it or loads a built tile
     public void BuildTile(int type, Quaternion rotation)
     {
         int tileType = 0;
@@ -102,7 +102,7 @@ public class Build : MonoBehaviour
 
         gameManager.updateTileArray(index, value);
     }
-
+    //Adds the current tile to an array that is later used for saving in the GameManager
     public int SaveTile(Vector2 tilePosition, int tileType, int tileRotation)
     {
         int tileInt = 0;
@@ -111,13 +111,13 @@ public class Build : MonoBehaviour
 
         return tileInt;
     }
-
+    //Takes a tile out of the saving array whenever it is deleted
     public void UnsaveTile(Vector3 position)
     {
         int index = ((int)position.x + 11 + ((int)position.y - 1) * 21) - 1;
         gameManager.updateTileArray(index, 0);
     }
-
+    //Runs when the player deletes a tile
     public void DeletedTile(GameObject tile, int tileType)
     {
         UnsaveTile(tile.transform.position);
